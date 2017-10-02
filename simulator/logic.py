58760d2c58c2f5051fab664635313ec67d96cdb3
philipperemy <premy@reactive.co.jp>
2017-10-02 19:22:07 +0900

    simulator

--- simulator/logic.py
+++ simulator/logic.py
@@ -1,3 +1,60 @@
-# will contain:
+# will contain=
 # - re player for bid-ask prices.
-# - basic in memory logic, database and so on
+# - basic in memory logic database and so on
+
+
+class UserAccount:
+    def __init__(self):
+        self.btc_available = 0.00000000
+        self.btc_balance = 0.00000000
+
+        self.btc_reserved = 0.00000000
+        self.btceur_fee = 0.25
+        self.btcusd_fee = 0.25
+
+        self.eth_available = 0.00000000
+        self.eth_balance = 0.00000000
+        self.eth_reserved = 0.00000000
+        self.ethbtc_fee = 0.00
+        self.etheur_fee = 0.00
+
+        self.ethusd_fee = 0.00
+        self.eur_available = 105
+
+        self.eur_balance = 105
+        self.eur_reserved = 0.00
+
+        self.eurusd_fee = 0.25
+        self.ltc_available = 0.00000000
+        self.ltc_balance = 0.00000000
+
+        self.ltc_reserved = 0.00000000
+        self.ltcbtc_fee = 0.25
+
+        self.ltceur_fee = 0.25
+        self.ltcusd_fee = 0.25
+
+        self.usd_available = 0.00
+        self.usd_balance = 0.00
+
+        self.usd_reserved = 0.00
+        self.xrp_available = 0.00000000
+
+        self.xrp_balance = 0.00000000
+        self.xrp_reserved = 0.00000000
+
+        self.xrpbtc_fee = 0.25
+        self.xrpeur_fee = 0.25
+        self.xrpusd_fee = 0.25
+
+        # first transaction is the payment.
+        self.transactions = [
+            {'fee': '0.00', 'btc_usd': '0.00', 'datetime': '2017-09-22 08:45:10', 'usd': 0.0, 'btc': 0.0, 'type': '0',
+             'id': 22034181, 'eur': '105.00'}]
+
+        # {'price': Decimal('2470.22'), 'currency_pair': 'BTC/EUR',
+        # 'datetime': datetime.datetime(2017, 9, 24, 10, 50, 50), 'amount': Decimal('0.00300000'),
+        # 'type': '0', 'id': '320422620'}
+        self.open_orders = []
+
+        self.order_statuses = []
--- simulator/main.py
+++ simulator/main.py
@@ -1,8 +1,16 @@
+import datetime
+from decimal import Decimal
+
 from flask import Flask, url_for
+from flask import request
 
 from connectivity.calls import DEFAULT_CURRENCY_PAIR
+from simulator import logic
 
 app = Flask(__name__)
+user = logic.UserAccount()
+
+BTC_EUR_FIXED_PRICE = 3000
 
 
 def has_no_empty_params(rule):
@@ -11,7 +19,7 @@ def has_no_empty_params(rule):
     return len(defaults) >= len(arguments)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def site_map():
     links = []
     for rule in app.url_map.iter_rules():
@@ -24,54 +32,187 @@ def site_map():
     return '<br/>'.join(sorted(['<a href="{0}">{0}</a> -> {1}()'.format(l, v) for (l, v) in links]))
 
 
-@app.route('/balance/')
+@app.route('/balance/', methods=['GET', 'POST'])
 def balance():
+    # {'btc_available': '0.00300000', 'btc_balance': '0.00300000',
+    # 'btc_reserved': '0.00000000', 'btceur_fee': '0.25', 'btcusd_fee': '0.25',
+    # 'eth_available': '0.00000000', 'eth_balance': '0.00000000', 'eth_reserved':
+    # '0.00000000', 'ethbtc_fee': '0.00', 'etheur_fee': '0.00',
+    # 'ethusd_fee': '0.00', 'eur_available': '95.72',
+    # 'eur_balance': '95.72', 'eur_reserved': '0.00',
+    # 'eurusd_fee': '0.25', 'ltc_available':
+    # '0.00000000', 'ltc_balance': '0.00000000',
+    # 'ltc_reserved': '0.00000000', 'ltcbtc_fee': '0.25',
+    # 'ltceur_fee': '0.25', 'ltcusd_fee': '0.25',
+    # 'usd_available': '0.00', 'usd_balance': '0.00',
+    # 'usd_reserved': '0.00', 'xrp_available': '0.00000000',
+    # 'xrp_balance': '0.00000000', 'xrp_reserved': '0.00000000',
+    # 'xrpbtc_fee': '0.25', 'xrpeur_fee': '0.25', 'xrpusd_fee': '0.25'}
     return 'Hello World!'
 
 
-@app.route('/buy/{}/'.format(DEFAULT_CURRENCY_PAIR))
+# curl -X POST http://127.0.0.1:5000/buy/btceur/ -d "{'amount': 0.003, 'key': '***', 'signature': '***', 'nonce': '1506251375795815'}"
+@app.route('/buy/{}/'.format(DEFAULT_CURRENCY_PAIR), methods=['GET', 'POST'])
 def buy():
-    return 'Hello World!'
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        if int(data['admin_resting_order']):
+            print('Order is resting.')
+            order_id = 320422620
+            user.open_orders[order_id] = {'price': Decimal(str(BTC_EUR_FIXED_PRICE)),
+                                          'currency_pair': 'BTC/EUR',
+                                          'datetime': datetime.datetime(2017, 9, 24, 10, 50, 50),
+                                          'amount': Decimal(data['amount']),
+                                          'type': '0',  # BUY
+                                          'id': str(order_id)}
+        else:
+            buy_market()
 
 
-@app.route('/sell/{}/'.format(DEFAULT_CURRENCY_PAIR))
+@app.route('/sell/{}/'.format(DEFAULT_CURRENCY_PAIR), methods=['GET', 'POST'])
 def sell():
-    return 'Hello World!'
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        if int(data['admin_resting_order']):
+            print('Order is resting.')
+            order_id = 320422620
+            user.open_orders[order_id] = {'price': Decimal(str(BTC_EUR_FIXED_PRICE)),
+                                          'currency_pair': 'BTC/EUR',
+                                          'datetime': datetime.datetime(2017, 9, 24, 10, 50, 50),
+                                          'amount': Decimal(data['amount']),
+                                          'type': '1',  # SELL
+                                          'id': str(order_id)}
+        else:
+            sell_market()
 
 
-@app.route('/buy/market/{}/'.format(DEFAULT_CURRENCY_PAIR))
+@app.route('/buy/market/{}/'.format(DEFAULT_CURRENCY_PAIR), methods=['GET', 'POST'])
 def buy_market():
-    return 'Hello World!'
+    # https://www.bitstamp.net/api/v2/buy/market/btceur/
+    # {'amount': 0.003, 'key': '***', 'signature': '***', 'nonce': '1506251375795815'}
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        btc_order_amount = float(data['amount'])
+
+        total_price_eur = btc_order_amount * BTC_EUR_FIXED_PRICE
+
+        user.btc_available += btc_order_amount
+        user.btc_balance += btc_order_amount
+
+        user.eur_available -= total_price_eur
+        user.eur_balance -= total_price_eur
+
+        total_fees = user.btceur_fee * total_price_eur
+        user.eur_available -= total_fees
+        user.eur_balance -= total_fees
+
+        order_id = 320464858
+        transaction_id = 22155667
+
+        user.transactions.append(
+            {'fee': str(total_fees),
+             'order_id': order_id,
+             'datetime': '2017-09-24 11:09:39',
+             'usd': 0.0,
+             'btc': str(btc_order_amount),
+             'btc_eur': BTC_EUR_FIXED_PRICE,
+             'type': '2',
+             'id': transaction_id,
+             'eur': str(-total_price_eur)})
+
+        user.order_statuses[order_id] = {'status': 'Finished', 'transactions': [
+            {'fee': str(total_fees),
+             'price': str(BTC_EUR_FIXED_PRICE),
+             'datetime': '2017-09-24 11:09:39',
+             'btc': '0.00300000',
+             'tid': transaction_id,
+             'type': 2,  # MARKET TRADE
+             'eur': str(total_price_eur)}]}
+
+    return 'Buy'
 
 
-@app.route('/sell/market/{}/'.format(DEFAULT_CURRENCY_PAIR))
+@app.route('/sell/market/{}/'.format(DEFAULT_CURRENCY_PAIR), methods=['GET', 'POST'])
 def sell_market():
-    return 'Hello World!'
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        btc_order_amount = float(data['amount'])
+
+        total_price_eur = btc_order_amount * BTC_EUR_FIXED_PRICE
+
+        user.btc_available -= btc_order_amount
+        user.btc_balance -= btc_order_amount
+
+        user.eur_available += total_price_eur
+        user.eur_balance += total_price_eur
+
+        total_fees = user.btceur_fee * total_price_eur
+        user.eur_balance -= total_fees
+        user.eur_balance -= total_fees
+
+        order_id = 22155667
+        transaction_id = 22155667
+
+        user.transactions.append(
+            {'fee': str(total_fees),
+             'order_id': order_id,
+             'datetime': '2017-09-24 11:09:39',
+             'usd': 0.0,
+             'btc': str(-btc_order_amount),
+             'btc_eur': BTC_EUR_FIXED_PRICE,
+             'type': '2',
+             'id': transaction_id,
+             'eur': str(total_price_eur)})
+
+        user.order_statuses[order_id] = {'status': 'Finished', 'transactions': [
+            {'fee': str(total_fees),
+             'price': str(BTC_EUR_FIXED_PRICE),
+             'datetime': '2017-09-24 11:09:39',
+             'btc': '0.00300000',
+             'tid': transaction_id,
+             'type': 2,  # MARKET TRADE
+             'eur': str(total_price_eur)}]}
+
+    return 'Sell'
 
 
-@app.route('/cancel_order/')
+@app.route('/cancel_order/', methods=['GET', 'POST'])
 def cancel_order():
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        order_id = data['id']
+        del user.open_orders[order_id]
     return 'Hello World!'
 
 
-@app.route('/order_status/')
+# {'id': '320464858', 'key': '***', 'signature': '***', 'nonce': '1506251383558164'}
+@app.route('/order_status/', methods=['GET', 'POST'])
 def order_status():
-    return 'Hello World!'
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        order_id = data['id']
+        return user.order_statuses[order_id]
 
 
-@app.route('/open_orders/all/')
+@app.route('/open_orders/all/', methods=['GET', 'POST'])
 def open_orders_all():
-    return 'Hello World!'
+    return NotImplementedError()
 
 
-@app.route('/transactions/{}/'.format(DEFAULT_CURRENCY_PAIR))
+@app.route('/transactions/{}/'.format(DEFAULT_CURRENCY_PAIR), methods=['GET', 'POST'])
 def transactions():
-    return 'Hello World!'
+    raise NotImplementedError()
 
 
-@app.route('/user_transactions/')
+@app.route('/user_transactions/', methods=['GET', 'POST'])
 def user_transactions():
-    return 'Hello World!'
+    return user.transactions[::-1]
 
 
 if __name__ == '__main__':
