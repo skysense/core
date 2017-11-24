9b6e6206363de652584f3a2d018d573cf2adb52e
jclune <jclune.berkeley@gmail.com>
2017-11-24 11:10:02 -0800

    training notebook, safer open of feeds.txt

--- bak/feeds.py
+++ bak/feeds.py
@@ -2,17 +2,25 @@ import feedparser
 
 from helpers.singleton_observable import SingletonObservable
 
-rss_feeds = open('feeds.txt').readlines()
-
 
 class NewsAPI(SingletonObservable):
     _instance = None
 
     def __init__(self):
         super().__init__(NewsAPI)
+        self.filename = '../connectivity/feeds.txt'
 
-    @staticmethod
-    def display_news():
+    def read_rss_feeds(self):
+        if not os.path.isfile(self.filename):
+            return {}  # non existing file.
+        if os.stat(self.filename).st_size == 0:
+            return {}  # empty file.
+        else:
+            rss_feeds = open(self.filename).readlines()
+            return rss_feeds
+
+    def display_news(self):
+        rss_feeds = self.read_rss_feeds()
         for rss_feed in rss_feeds:
 
             d = feedparser.parse(rss_feed)
